459b34b8fccae15094db1f216357076475a2aef4
Sorush Bahariyan <sorush.bahariyan@gmail.com>
2025-09-13 15:33:28 +0330

    implemented withdraw repository

--- lib/withdraws/repository/exportables.go
+++ lib/withdraws/repository/exportables.go
@@ -1,1 +1,42 @@
 package repository
+
+import (
+	"context"
+	"wallet/lib/withdraws/enums"
+	"wallet/lib/withdraws/repository/internal"
+
+	"gorm.io/gorm"
+)
+
+type Withdrawal = internal.Withdrawal
+
+type Repo interface {
+	Create(context.Context, *Withdrawal) error
+	Update(context.Context, *Withdrawal) error
+	GetUnFinishedWithdraws(ctx context.Context, IDPrefix string, bankType enums.BankType) ([]Withdrawal, error)
+
+	GetDBTransaction() *gorm.DB
+	Commit() error
+	RollBack() error
+}
+
+type RepoFactory interface {
+	New(tx *gorm.DB) Repo
+}
+
+func NewFactory(db *gorm.DB) RepoFactory {
+	return &repoFactory{
+		db: db,
+	}
+}
+
+type repoFactory struct {
+	db *gorm.DB
+}
+
+func (rf *repoFactory) New(tx *gorm.DB) Repo {
+	if tx == nil {
+		tx = rf.db.Begin()
+	}
+	return internal.NewWithdrawalRepo(tx)
+}
--- lib/withdraws/repository/internal/repo.go
+++ lib/withdraws/repository/internal/repo.go
@@ -1,1 +1,52 @@
 package internal
+
+import (
+	"context"
+	"wallet/lib/withdraws/enums"
+
+	"gorm.io/gorm"
+)
+
+type withdrawalRepo struct {
+	tx *gorm.DB
+}
+
+func NewWithdrawalRepo(tx *gorm.DB) *withdrawalRepo {
+	return &withdrawalRepo{tx: tx}
+}
+
+func (r *withdrawalRepo) Create(ctx context.Context, w *Withdrawal) error {
+	return r.tx.WithContext(ctx).Create(w).Error
+}
+
+func (r *withdrawalRepo) Update(ctx context.Context, w *Withdrawal) error {
+	return r.tx.WithContext(ctx).Save(w).Error
+}
+
+// GetUnFinishedWithdraws returns withdrawals with status "new" or "sent"
+func (r *withdrawalRepo) GetUnFinishedWithdraws(ctx context.Context, idPrefix string, bankType enums.BankType) ([]Withdrawal, error) {
+	var withdraws []Withdrawal
+
+	query := r.tx.WithContext(ctx).Model(&Withdrawal{}).
+		Where("status IN ?", []enums.PayoutStatus{enums.NEW, enums.SENT}).
+		Where("bank = ?", bankType)
+
+	if idPrefix != "" {
+		query = query.Where("CAST(id AS TEXT) LIKE ?", idPrefix+"%")
+	}
+
+	err := query.Find(&withdraws).Error
+	return withdraws, err
+}
+
+func (r *withdrawalRepo) GetDBTransaction() *gorm.DB {
+	return r.tx
+}
+
+func (r *withdrawalRepo) Commit() error {
+	return r.tx.Commit().Error
+}
+
+func (r *withdrawalRepo) RollBack() error {
+	return r.tx.Rollback().Error
+}
