c5bc7b99dcc4c64b1ec68fb8759e4e9adc6fe9d2
Sorush Bahariyan <sorush.bahariyan@gmail.com>
2025-09-14 02:42:55 +0330

    withdraws: implemented bank woker

---
+++ lib/withdraws/bank_worker.go
@@ -0,0 +1,125 @@
+package withdraws
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"wallet/lib/withdraws/enums"
+	"wallet/lib/withdraws/integrations"
+)
+
+type jobType int
+
+const (
+	jobSend jobType = iota
+	jobCheck
+)
+
+type job struct {
+	ctx context.Context
+	wd  *Withdrawal
+	t   jobType
+}
+
+type worker struct {
+	updateStatus func(context.Context, *Withdrawal) error
+	concurrency  int
+	backOff      time.Duration
+	retryCount   int
+	client       integrations.BankClient
+
+	jobs chan job
+	done chan struct{}
+}
+
+func (w *worker) Run(ctx context.Context) {
+	for i := 0; i < w.concurrency; i++ {
+		go func() {
+			for {
+				select {
+				case j := <-w.jobs:
+					switch j.t {
+					case jobSend:
+						_ = w.doSend(j.ctx, j.wd)
+					case jobCheck:
+						_ = w.doCheck(j.ctx, j.wd)
+					}
+				case <-ctx.Done():
+					return
+				case <-w.done:
+					return
+				}
+			}
+		}()
+	}
+}
+
+func (w *worker) Stop() {
+	close(w.done)
+}
+
+func (w *worker) doWithRetry(ctx context.Context, fn func() error) error {
+	var err error
+	for i := 0; i < w.retryCount; i++ {
+		err = fn()
+		if err == nil {
+			return nil
+		}
+		if errors.Is(ctx.Err(), context.Canceled) {
+			return ctx.Err()
+		}
+		select {
+		case <-time.After(w.backOff):
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+	return err
+}
+
+func (w *worker) doSend(ctx context.Context, wd *Withdrawal) error {
+	var status enums.PayoutStatus
+	err := w.doWithRetry(ctx, func() error {
+		var e error
+		status, e = w.client.Send(wd.Iban, wd.Amount, wd.ID.String())
+		return e
+	})
+	if err != nil {
+		return err
+	}
+	wd.Status = status
+	return w.updateStatus(ctx, wd)
+}
+
+func (w *worker) doCheck(ctx context.Context, wd *Withdrawal) error {
+	var status enums.PayoutStatus
+	err := w.doWithRetry(ctx, func() error {
+		var e error
+		status, e = w.client.GetStatus(wd.ID.String())
+		return e
+	})
+	if err != nil {
+		return err
+	}
+	wd.Status = status
+	return w.updateStatus(ctx, wd)
+}
+
+func (w *worker) SendToBank(ctx context.Context, wd *Withdrawal) error {
+	select {
+	case w.jobs <- job{ctx: ctx, wd: wd, t: jobSend}:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+func (w *worker) GetStatus(ctx context.Context, wd *Withdrawal) error {
+	select {
+	case w.jobs <- job{ctx: ctx, wd: wd, t: jobCheck}:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
--- lib/withdraws/exporatables.go
+++ lib/withdraws/exporatables.go
@@ -2,7 +2,9 @@ package withdraws
 
 import (
 	"context"
+	"time"
 	"wallet/lib/core"
+	"wallet/lib/withdraws/integrations"
 	"wallet/lib/withdraws/repository"
 )
 
@@ -22,5 +24,30 @@ func NewService(
 	return &service{
 		coreRepoFactory:     coreRepoFactory,
 		withdrawRepoFactory: withdrawRepoFactory,
+	}
+}
+
+type Worker interface {
+	SendToBank(context.Context, *Withdrawal) error
+	GetStatus(context.Context, *Withdrawal) error
+	Run(ctx context.Context)
+	Stop()
+}
+
+func NewWorker(
+	updateStatus func(context.Context, *Withdrawal) error,
+	concurrency int,
+	backOff time.Duration,
+	retryCount int,
+	client integrations.BankClient,
+) Worker {
+	return &worker{
+		updateStatus: updateStatus,
+		concurrency:  concurrency,
+		backOff:      backOff,
+		retryCount:   retryCount,
+		client:       client,
+		jobs:         make(chan job),
+		done:         make(chan struct{}),
 	}
 }
